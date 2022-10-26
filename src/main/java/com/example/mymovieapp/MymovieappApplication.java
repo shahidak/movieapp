6bcec6d826d64050bc8cff0a0b0ba5cf87789cf3
Shahid <shahid@rnmkrs.com>
2022-10-26 12:59:07 +0800

    first commit

---
+++ src/main/java/com/example/mymovieapp/MymovieappApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymovieappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymovieappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mymovieapp/MymovieappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymovieappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
